091fe17fca2393035c79d97b78aa92adabdb6804
wanghao <wanghao@zettakit.com>
2018-03-23 16:33:43 +0800

    Support less and add relative webpack config

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.less";
+import less from "./App.less";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={less.App}>
+        <header className={less["App-header"]}>
+          <img src={logo} className={less["App-logo"]} alt="logo" />
+          <h1 className={less["App-title"]}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={less["App-intro"]}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
